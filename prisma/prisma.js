6a492e5983d461fbf9eb190a01349b9bd9ea3818
Nathan <ncmoseley@gmail.com>
2021-09-25 12:08:09 -0700

    Lets see if this deploys

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,18 +1,38 @@
+/* eslint-disable no-void */
 /* eslint-disable no-undef */
 import { PrismaClient } from '@prisma/client';
 
-let prisma = {};
+// let prisma = {};
 
-if (process.env.NODE_ENV !== "production") {
-    // prisma = new PrismaClient({ log: [ "query", "info", "warn" ] });
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        console.log("GLOBAL PRISMA:");
-        global.prisma = new PrismaClient({ log: [ "query", "info", "warn" ] });
+// if (process.env.NODE_ENV !== "production") {
+//     // prisma = new PrismaClient({ log: [ "query", "info", "warn" ] });
+//     prisma = new PrismaClient();
+// } else {
+//     if (!global.prisma) {
+//         console.log("GLOBAL PRISMA:");
+//         global.prisma = new PrismaClient({ log: [ "query", "info", "warn" ] });
+//     }
+
+//     prisma = global.prisma;
+// }
+
+// export default prisma;
+
+function createPrisma() {
+    if (process.env.NODE_ENV === "production") {
+        return new PrismaClient({ log: [ "warn", "error" ] });
     }
 
-    prisma = global.prisma;
+    // Ensure that previous prisma instance is disconnected.
+    if ("prisma" in globalThis && "$disconnect" in globalThis.prisma) {
+        void globalThis.prisma.$disconnect();
+    }
+
+    globalThis.prisma = new PrismaClient({
+        log: [ "info", "query", "warn", "error" ]
+    });
+
+    return globalThis.prisma;
 }
 
-export default prisma;
+export default prisma = createPrisma();
