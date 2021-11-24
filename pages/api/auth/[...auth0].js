27467e06b4748086663af653c27c0d34255afd8b
Ben McLoughlin <benmcl@shaw.ca>
2021-11-24 13:07:39 -0800

    Nov24-handleAuth() was returning to localhost, was that the issue?

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -3,7 +3,7 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 export default handleAuth({
     async login(req, res) {
         await handleLogin(req, res, {
-            returnTo: 'http://localhost:3000/shopper/onboard'
+            returnTo: 'https://bens-remote-shop-hopper-api.vercel.app/shopper/onboard'
         });
     }
 });
