03960cd077e9871d0cb08110d3a98822848033e8
Ben McLoughlin <benmcl@shaw.ca>
2021-11-24 13:16:07 -0800

    Nov24 - removed redirect in handleAuth to see if that will work

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,9 +1,10 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-    async login(req, res) {
-        await handleLogin(req, res, {
-            returnTo: 'https://bens-remote-shop-hopper-api.vercel.app/shopper/onboard'
-        });
-    }
-});
+// export default handleAuth({
+//     async login(req, res) {
+//         await handleLogin(req, res, {
+//             returnTo: 'https://bens-remote-shop-hopper-api.vercel.app/shopper/onboard'
+//         });
+//     }
+// });
+export default handleAuth();
