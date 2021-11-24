b41fa44eb1283d2149d5e94e33ad5858056b8722
Ben McLoughlin <benmcl@shaw.ca>
2021-11-24 14:39:57 -0800

    Nov24 - Auth0, can access login panel but get checks.state argument is missing'

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,10 +1,9 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-// export default handleAuth({
-//     async login(req, res) {
-//         await handleLogin(req, res, {
-//             returnTo: 'https://bens-remote-shop-hopper-api.vercel.app/shopper/onboard'
-//         });
-//     }
-// });
-export default handleAuth();
+export default handleAuth({
+    async login(req, res) {
+        await handleLogin(req, res, {
+            returnTo: 'https://bens-remote-shop-hopper-api.vercel.app/shopper/onboard'
+        });
+    }
+});
